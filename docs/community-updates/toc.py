df41a0c8a6ed4270fb3e9b0bcf43fb47a1cb497b
Dyvinia <dulana57@gmail.com>
2024-04-17 18:55:25 -0400

    shorten toc version of headers

--- docs/community-updates/toc.py
+++ docs/community-updates/toc.py
@@ -4,7 +4,7 @@ def on_page_content(html, page, **kwargs):
     if page.file.src_uri == "community-updates/v10.md":
         for link in page.toc:
             link.children.append(AnchorLink('Kyber V2', 'kyber-v2', 0))
-            link.children.append(AnchorLink('Daytime And Weather Variants', 'weather', 0))
+            link.children.append(AnchorLink('Weather Variants', 'weather', 0))
             link.children.append(AnchorLink('Blasters', 'blasters', 0))
             link.children.append(AnchorLink('Appearances', 'appearances', 0))
             link.children.append(AnchorLink('Heroes', 'heroes', 0))
