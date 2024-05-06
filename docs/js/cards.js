11c7de1697bba65e0c06d0a136689b8372c4f84d
Dyvinia <dulana57@gmail.com>
2024-05-06 05:36:13 -0400

    add the event listener to the container not the whole document smh

--- docs/js/cards.js
+++ docs/js/cards.js
@@ -13,7 +13,7 @@ if (container) {
     });
     
     
-    document.addEventListener('swiped-left', function(e) {
+    container.addEventListener('swiped-left', function(e) {
         let currentIndex = -1;
         let cards = container.querySelectorAll(".card");
         
@@ -36,7 +36,7 @@ if (container) {
         }
     });
     
-    document.addEventListener('swiped-right', function(e) {
+    container.addEventListener('swiped-right', function(e) {
         let currentIndex = -1;
         let cards = container.querySelectorAll(".card");
 
