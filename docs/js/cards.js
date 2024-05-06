64a9f03f12b17631c0b843dcf70097acf02e2475
Dyvinia <dulana57@gmail.com>
2024-05-06 05:32:03 -0400

    check if container exists when running cards js

--- docs/js/cards.js
+++ docs/js/cards.js
@@ -1,62 +1,63 @@
 const container = document.querySelector(".container-cards");
-
-container.addEventListener("click", (e) => {
-    const target = e.target.closest(".card");
-
-    if (!target) return;
-
-    container.querySelectorAll(".card").forEach((card) => {
-        card.classList.remove("active");
-    });
-
-    target.classList.add("active");
-});
-
-
-document.addEventListener('swiped-left', function(e) {
-    let index = -1;
-
-    let i = 0;
-    let cards = container.querySelectorAll(".card");
+if (container) {
+    container.addEventListener("click", (e) => {
+        const target = e.target.closest(".card");
     
-    cards.forEach((card) => {
-        if (card.classList.contains("active"))
-            index = i;
-        i++;
+        if (!target) return;
+    
+        container.querySelectorAll(".card").forEach((card) => {
+            card.classList.remove("active");
+        });
+    
+        target.classList.add("active");
     });
-
-    if (index < (cards.length - 1)) {
-        i = 0;
+    
+    
+    document.addEventListener('swiped-left', function(e) {
+        let index = -1;
+    
+        let i = 0;
+        let cards = container.querySelectorAll(".card");
+        
         cards.forEach((card) => {
-            if (index == i)
-                card.classList.remove("active");
-            if ((index + 1) == i)
-                card.classList.add("active");
+            if (card.classList.contains("active"))
+                index = i;
             i++;
         });
-    }
-});
-
-document.addEventListener('swiped-right', function(e) {
-    let index = -1;
-
-    let i = 0;
-    let cards = container.querySelectorAll(".card");
     
-    cards.forEach((card) => {
-        if (card.classList.contains("active"))
-            index = i;
-        i++;
+        if (index < (cards.length - 1)) {
+            i = 0;
+            cards.forEach((card) => {
+                if (index == i)
+                    card.classList.remove("active");
+                if ((index + 1) == i)
+                    card.classList.add("active");
+                i++;
+            });
+        }
     });
-
-    if (index > 0) {
-        i = 0;
+    
+    document.addEventListener('swiped-right', function(e) {
+        let index = -1;
+    
+        let i = 0;
+        let cards = container.querySelectorAll(".card");
+        
         cards.forEach((card) => {
-            if (index == i)
-                card.classList.remove("active");
-            if ((index - 1) == i)
-                card.classList.add("active");
+            if (card.classList.contains("active"))
+                index = i;
             i++;
         });
-    }
-});
+    
+        if (index > 0) {
+            i = 0;
+            cards.forEach((card) => {
+                if (index == i)
+                    card.classList.remove("active");
+                if ((index - 1) == i)
+                    card.classList.add("active");
+                i++;
+            });
+        }
+    });
+}
