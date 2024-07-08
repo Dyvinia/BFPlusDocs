f88f7770da369dac52f3712bdeaff62d7067b4e3
Dyvinia <dulana57@gmail.com>
2024-07-07 20:57:13 -0400

    simplify code even more

--- docs/js/cards.js
+++ docs/js/cards.js
@@ -1,36 +1,34 @@
 for (const container of document.querySelectorAll(".container-cards")) {
-    if (container) {
-        container.addEventListener("click", (e) => {
-            const target = e.target.closest(".card");
-        
-            if (!target) return;
-        
-            container.querySelectorAll(".card").forEach((card) => {
-                card.classList.remove("active");
-            });
-        
-            target.classList.add("active");
+    container.addEventListener("click", (e) => {
+        const target = e.target.closest(".card");
+    
+        if (!target) return;
+    
+        container.querySelectorAll(".card").forEach((card) => {
+            card.classList.remove("active");
         });
-        
-        
-        container.addEventListener('swiped-left', () => {
-            let cards = [...container.querySelectorAll(".card")];
-            let currentIndex = cards.findIndex(c => c.classList.contains("active"));
-        
-            if (currentIndex < (cards.length - 1)) {
-                cards[currentIndex].classList.remove("active");
-                cards[currentIndex + 1].classList.add("active");
-            }
-        });
-        
-        container.addEventListener('swiped-right', () => {
-            let cards = [...container.querySelectorAll(".card")];
-            let currentIndex = cards.findIndex(c => c.classList.contains("active"));
-        
-            if (currentIndex > 0) {
-                cards[currentIndex].classList.remove("active");
-                cards[currentIndex - 1].classList.add("active");
-            }
-        });
-    }
+    
+        target.classList.add("active");
+    });
+    
+    
+    container.addEventListener('swiped-left', () => {
+        let cards = [...container.querySelectorAll(".card")];
+        let currentIndex = cards.findIndex(c => c.classList.contains("active"));
+    
+        if (currentIndex < (cards.length - 1)) {
+            cards[currentIndex].classList.remove("active");
+            cards[currentIndex + 1].classList.add("active");
+        }
+    });
+    
+    container.addEventListener('swiped-right', () => {
+        let cards = [...container.querySelectorAll(".card")];
+        let currentIndex = cards.findIndex(c => c.classList.contains("active"));
+    
+        if (currentIndex > 0) {
+            cards[currentIndex].classList.remove("active");
+            cards[currentIndex - 1].classList.add("active");
+        }
+    });
 }
